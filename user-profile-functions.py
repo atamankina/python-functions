caa9155a0be5d5888709e5ec233d5f644b52bef8
Galina Atamankina <g.atamankina@gmail.com>
2025-03-28 13:08:41 +0100

    Adding the user profile function.

---
+++ user-profile-functions.py
@@ -0,0 +1,14 @@
+def create_user_profile(*kwargs):
+    profile = {}
+
+    for key, value in kwargs.items():
+        profile[key] = value
+
+    return profile
+
+
+alice = create_user_profile(name="Alice", age=33, city="New York")
+bob = create_user_profile(name="Bob", last_name="Smith", age=22, phone_number=1234567890)
+
+print(alice)
+print(bob)
