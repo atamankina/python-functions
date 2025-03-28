b2794948698aa6bd547234a9766b8bc8d5c77d3f
Galina Atamankina <g.atamankina@gmail.com>
2025-03-28 11:40:02 +0100

    Adding the first calculation functions.

---
+++ calculate-functions.py
@@ -0,0 +1,15 @@
+def add_numbers(number1, number2):
+    sum = number1 + number2
+    return sum
+
+a = 10
+b = 20
+
+add_numbers(number1=a, number2=b)
+add_numbers(a, b)
+
+c = add_numbers(a, b)
+print(f"Die Summe von a={a} und b={b} ist {c}.")
+
+d = add_numbers(c, 5)
+print(f"Die Summe von c={c} und 5 ist {d}.")
