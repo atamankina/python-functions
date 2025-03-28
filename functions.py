079971e120c434d02844db0f83c6b85c4fc39e26
Galina Atamankina <g.atamankina@gmail.com>
2025-03-28 11:15:29 +0100

    Adding the greet_person function.

--- functions.py
+++ functions.py
@@ -4,13 +4,30 @@ def greet():
 
 greet()
 
-# some other code
-print("Other Code 1.")
+name1 = "Thor"
+name2 = "Joel"
+list_of_names = ["Katja", "Olga", "Tim"]
 
-greet()
+# name = parameter
+def greet_person(name):
+    print("Hello, ", name)
+    print("End of the greet_person function.")
 
-# some other code
-print("Other Code 2.")
+# the named argument
+greet_person(name=name1)
+# name1 = argument
+greet_person(name1)
+# name2 = argument
+greet_person(name2)
 
-greet()
+# the named argument
+greet_person(name="Najat")
+greet_person(name="Zito")
+
+# a value as a function argument
+greet_person("Hanno")
+
+for name in list_of_names:
+    greet_person(name)
+
 
